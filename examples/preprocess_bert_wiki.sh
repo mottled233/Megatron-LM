1dd3e15aa95434a2310b5abc666f9d1fdca1b9c7
mottled233 <631061840@qq.com>
2020-10-29 16:19:55 +0800

    数据预处理脚本

---
+++ examples/preprocess_bert_wiki.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VOCAB_FILE=bert-vocab.txt
+INPUT_DIR=
+OUTPUT_PREFIX=
+NUM_OF_WORKER=16
+
+python preprocess_data.py \
+       --input $INPUT_DIR \
+       --json-keys text \
+       --split-sentences \
+       --tokenizer-type BertWordPieceCase \
+       --vocab-file $VOCAB_FILE \
+       --output-prefix $OUTPUT_PREFIX \
+       --workers $NUM_OF_WORKER
