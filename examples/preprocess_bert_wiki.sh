246bbce1328593ef1bb81222aa45ad4d063b30d2
mottled233 <631061840@qq.com>
2020-10-29 16:38:59 +0800

    数据预处理脚本

--- examples/preprocess_bert_wiki.sh
+++ examples/preprocess_bert_wiki.sh
@@ -5,7 +5,7 @@ INPUT_DIR=/cfs/aidenliang/corpus/wiki_data/text/AA
 OUTPUT_PREFIX=/cfs/aidenliang/corpus/wiki_data/db/wiki
 NUM_OF_WORKER=16
 
-python tools\preprocess_data.py \
+python tools/preprocess_data.py \
        --input $INPUT_DIR \
        --json-keys text \
        --split-sentences \
