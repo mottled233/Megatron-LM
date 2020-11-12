092c87cd8d587d8d188063ea5300daeea1b4e080
mottled233 <631061840@qq.com>
2020-11-12 19:21:01 +0800

    fix fp16 dynamic loss scale

--- openweb_unzip.py
+++ openweb_unzip.py
@@ -1,8 +1,9 @@
 import os
 import tqdm
 
-input_dir = "/cfs/aidenliang/corpus/openwebtxt"
+input_dir = "/cfs/aidenliang/corpus/openwebtext"
 for parent, dirnames, filenames in os.walk(input_dir):
     filenames = list(filenames)
     for filename in tqdm.tqdm(filenames):
-        os.system(f"xz -d {filename}")
+        current = os.path.join(parent, filename)
+        os.system(f"xz -d {current}")
