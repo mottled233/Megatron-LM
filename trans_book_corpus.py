75c5bbf26186fcfe3d5b725ab14467bf9381a80f
mottled233 <631061840@qq.com>
2020-11-13 16:22:34 +0800

    fix fp16 dynamic loss scale

--- trans_book_corpus.py
+++ trans_book_corpus.py
@@ -2,6 +2,9 @@ import json
 import os
 import argparse
 from tqdm import tqdm
+import nltk
+import multiprocessing
+from functools import partial
 
 
 def parse_args():
@@ -13,44 +16,72 @@ def parse_args():
                        help='Path to output book corpus')
     group.add_argument('--remove-lines', type=int, default=60,
                        help='Skip the first n lines')
+    group.add_argument('--split-by', type=str, default="sentence",
+                       choices=['sentence', 'paragraph'])
     group.add_argument('--remove-empty-lines', type=bool, default=True)
     group.add_argument('--json-key', type=str, default="text")
     group.add_argument('--log-step', type=int, default=100)
+    group.add_argument('--max-seq-len', type=int, default=600)
     group.add_argument('--buff_file', type=int, default=1000)
+    group.add_argument('--num_of_workers', type=int, default=1)
     args = parser.parse_args()
     return args
+
+
+def para_splitter(text):
+    def no_blank(string):
+        string = string.replace("\n", "")
+        return len(string) > 0
+    return filter(no_blank, text.split("\n"))
+
+
+def process_book(filename, parent_dir, args, splitter):
+    current = os.path.join(parent_dir, filename)
+    buff = []
+    with open(current, 'r', encoding='utf-8') as f1:
+        lines = f1.readlines()
+    file = ""
+    sub_file = ""
+    for idx, line in enumerate(lines):
+        if idx < args.remove_lines:
+            continue
+        if args.remove_empty_lines and line == "\n":
+            continue
+        file += line
+    file = splitter(file)
+    for line in file:
+        sub_file += line
+        if len(sub_file) >= args.max_seq_len:
+            json_data = {args.json_key: sub_file}
+            buff.append(json.dumps(json_data))
+            sub_file = ""
+    with open(f"{args.output_dir}/books_{filename}.json", 'w', encoding='utf-8') as out_f:
+        out_f.write("\n".join(buff))
+
+    return 1
 
 
 def main():
     args = parse_args()
 
-    file_count = 0
-    count = 0
-    buff = []
+    if args.split_by == "paragraph":
+        splitter = para_splitter
+    elif args.split_by == "sentence":
+        splitter = nltk.load("tokenizers/punkt/english.pickle")
+    else:
+        splitter = None
+
+    if args.num_of_works > 1:
+        pool = multiprocessing.Pool(args.num_of_works)
+
     for parent, dirnames, filenames in tqdm(os.walk(args.input)):
-        for filename in filenames:
-            current = os.path.join(parent, filename)
-            with open(current, 'r', encoding='utf-8') as f1:
-                lines = f1.readlines()
-            file = ""
-            for idx, line in enumerate(lines):
-                if idx < args.remove_lines:
-                    continue
-                if args.remove_empty_lines and line == "\n":
-                    continue
-                file += line
-
-            json_data = {args.json_key: file}
-            buff.append(json.dumps(json_data))
-            count += 1
-            if count != 0 and count % args.log_step == 0:
-                print(f"{count} books handled...")
-
-            if len(buff) % args.buff_file == 0:
-                with open(f"{args.output_dir}/books_{file_count}.json", 'w', encoding='utf-8') as out_f:
-                    out_f.write("\n".join(buff))
-                file_count += 1
-                buff = []
+        parser = partial(process_book, parent_dir=parent, args=args, splitter=splitter)
+        if args.num_of_works > 1:
+            for _ in tqdm(pool.imap(parser, filenames)):
+                pass
+        else:
+            for file in tqdm(filenames):
+                parser(file)
 
 
 if __name__ == "__main__":
