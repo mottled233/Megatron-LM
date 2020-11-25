6d22e1d6a441b3deabd57f7d5c76cece232c6848
mottled233 <631061840@qq.com>
2020-11-25 11:08:27 +0800

    deep speed

--- tasks/squad/data.py
+++ tasks/squad/data.py
@@ -39,7 +39,7 @@ def load_and_cache_examples(args, tokenizer, stage="train", output_examples=Fals
         print_rank_0(f"Creating features from dataset file at {input_dir}")
 
         processor = SquadV1Processor()
-        if stage not in ["train", "dev"]:
+        if stage != "test":
             examples = processor.get_train_examples(args.data_dir, filename=args.train_data)
         else:
             examples = processor.get_dev_examples(args.data_dir, filename=args.valid_data)
