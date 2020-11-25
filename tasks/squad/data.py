7290866b562c2a5a85ff8c7f6a6562c8d7797c5b
mottled233 <631061840@qq.com>
2020-11-25 16:00:17 +0800

    deep speed

--- tasks/squad/data.py
+++ tasks/squad/data.py
@@ -134,11 +134,11 @@ def compute_predictions_logits(
 ):
     """Write final predictions to the json file and log-odds of null if needed."""
     if output_prediction_file:
-        logger.info(f"Writing predictions to: {output_prediction_file}")
+        print(f"Writing predictions to: {output_prediction_file}")
     if output_nbest_file:
-        logger.info(f"Writing nbest to: {output_nbest_file}")
+        print(f"Writing nbest to: {output_nbest_file}")
     if output_null_log_odds_file and version_2_with_negative:
-        logger.info(f"Writing null_log_odds to: {output_null_log_odds_file}")
+        print(f"Writing null_log_odds to: {output_null_log_odds_file}")
 
     example_index_to_features = collections.defaultdict(list)
     for feature in all_features:
