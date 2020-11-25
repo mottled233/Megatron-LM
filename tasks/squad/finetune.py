fbce0696fd86c3295de47c2ef0fa1068fe1ca6c0
mottled233 <631061840@qq.com>
2020-11-25 15:35:00 +0800

    deep speed

--- tasks/squad/finetune.py
+++ tasks/squad/finetune.py
@@ -158,9 +158,9 @@ def metrics_func_provider():
 
     args.test_batch_size_ = args.test_batch_size
     test_dataloader = make_data_loader(test_dataset, batch_size=args.test_batch_size_)
-    for iteration_, batch in enumerate(test_features):
+    for iteration_, batch in enumerate(test_dataloader):
         if iteration_ < 3:
-            print(batch.unique_id)
+            print(f"rank {mpu.get_data_parallel_rank()} batch feature {batch[3]}")
 
     def test_model_func(model, epoch=-1, output_predictions=True):
         model.eval()
