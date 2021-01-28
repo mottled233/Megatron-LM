c2f607f1dc866271b02fa02d3f89e953b8ca98e2
mottled233 <631061840@qq.com>
2021-01-28 16:19:03 +0800

    111

---
+++ trans_json_2_plain.py
@@ -0,0 +1,46 @@
+import json
+import os
+import time
+import argparse
+from tqdm import tqdm
+import nltk
+import multiprocessing
+from functools import partial
+from tools.preprocess_data import CustomLanguageVars
+from trans_book_corpus import whitespace_tokenize, para_splitter
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    group = parser.add_argument_group(title='input data')
+    group.add_argument('--input', type=str, required=True,
+                       help='Path to input wiki corpus')
+    group.add_argument('--output', type=str, required=True,
+                       help='Path to output wiki corpus')
+    group.add_argument('--json-key', type=str, default="text")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    # if args.num_of_workers > 1:
+    #     pool = multiprocessing.Pool(args.num_of_workers)
+
+    docs = []
+
+    for parent, dirnames, filenames in tqdm(os.walk(args.input)):
+        for filename in filenames:
+            with open(os.path.join(parent, filename), 'r', encoding='utf-8') as f1:
+                for json_line in f1:
+                    text = json.loads(json_line)[args.json_key].replace('\n', '')
+                    docs.append(text)
+    with open(os.path.join(args.output), 'w', encoding='utf-8') as f:
+        for doc in docs:
+            f.write(doc + "\n\n")
+
+
+if __name__ == "__main__":
+    main()
